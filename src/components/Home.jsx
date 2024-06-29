134aabe0796d24bbe2988aca22dce7c7f80fdb59
WebDevHarsha <vasriharsha@gmail.com>
2024-06-29 17:21:44 +0530

    some changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -17,7 +17,7 @@ const Home = () => {
             Heyo, This is <span className="bg-gradient-to-r from-yellow-500  to-orange-400 inline-block text-transparent bg-clip-text">Sri Harsha</span>! 👋
           </h1>
           <h1 className="text-xl md:text-xl font-bold drop-shadow-[2px_2px_2px_black]">
-          A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nextjs / Nodejs / React Native and some other cool libraries and frameworks.
+          A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with  Reactjs / Nextjs / Nodejs / React Native and some other cool libraries and frameworks.
           </h1>
         </div>
       </div>
